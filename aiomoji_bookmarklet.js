709aaf8918dd918708cd2ebd8f972778fdbc1b68
rtuna <38296319+rtunaboss@users.noreply.github.com>
2020-02-01 15:12:57 +0100

    Added Shopify support

--- aiomoji_bookmarklet.js
+++ aiomoji_bookmarklet.js
@@ -1,12 +1,14 @@
-javascript: (function () { 
-    let stringUrl = String(window.location.href);
-
-    // add a try catch here, includes tends to crash sometimes
-    try {
-        if (String(stringUrl).toLowerCase().includes("solebox")) {
-            document.location = `https://www.aiomoji.io/quicktask/add?storetype=solebox&url=${stringUrl}`;
-        };
-    } catch (err) {
-        // do nothing - it might crash if it doesn't find solebox
-    };
-})();
+javascript: (function () 
+{ var url = window.location.href;
+    stringUrl = String(url);
+    if (stringUrl.includes("solebox"))
+    {
+        stringUrl = "https://www.aiomoji.io/quicktask/add?quicktask=solebox:" + stringUrl;
+        document.location = stringUrl;
+    }
+    else
+    {
+        stringUrl = "https://www.aiomoji.io/quicktask/add?quicktask=" + stringUrl;
+        document.location = stringUrl;
+    }
+})()
